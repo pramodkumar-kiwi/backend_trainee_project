98cf05909861c515215a4c987e04924572b983ec
ayushgupta-kiwi <ayush.gupta@kiwitech.com>
2023-03-02 18:17:33 +0530

    updated

--- account/views.py
+++ account/views.py
@@ -2,10 +2,11 @@
 view for SignupView
 
 """
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import SignupSerializer
+from .serializers import SignupSerializer, SigninSerializer
 from .models import User
 
 
@@ -33,3 +34,26 @@ class SignupView(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class SigninView(viewsets.ViewSet):
+    """
+    Allow only authenticated user to signin.
+    If the user is valid provide him the access and refresh token
+    and save it to the database.
+    """
+    permission_classes = [permissions.AllowAny]
+    serializer_class = SigninSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.validated_data['user']
+
+            refresh = RefreshToken.for_user(user)
+
+            user_token = User.objects.get(id=user.id)
+            user_token.token = str(refresh.access_token)
+            user_token.save()
+
+            return Response({'access': str(refresh.access_token), 'refresh': str(refresh)})
+        return Response(status=status.HTTP_400_BAD_REQUEST)
